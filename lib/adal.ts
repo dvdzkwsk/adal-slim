88feee30ca237d62ff2196f7ad137451f1de58c5
David Zukowski <david@zuko.me>
2020-05-15 00:45:03 -0500

    Remove window.crypto polyfill from _guid()

--- lib/adal.ts
+++ lib/adal.ts
@@ -1586,19 +1586,6 @@ export class Adal {
     }
 
     /**
-     * Converts decimal value to hex equivalent
-     * @ignore
-     */
-    _decimalToHex(number: number) {
-        var hex = number.toString(16)
-
-        while (hex.length < 2) {
-            hex = "0" + hex
-        }
-        return hex
-    }
-
-    /**
      * Generates RFC4122 version 4 guid (128 bits)
      * @ignore
      */
@@ -1625,60 +1612,43 @@ export class Adal {
         // y values are 8, 9, A, B
         // @ts-expect-error
         var cryptoObj = window.crypto || window.msCrypto // for IE 11
-        if (cryptoObj && cryptoObj.getRandomValues) {
-            var buffer = new Uint8Array(16)
-            cryptoObj.getRandomValues(buffer)
-            //buffer[6] and buffer[7] represents the time_hi_and_version field. We will set the four most significant bits (4 through 7) of buffer[6] to represent decimal number 4 (UUID version number).
-            buffer[6] |= 0x40 //buffer[6] | 01000000 will set the 6 bit to 1.
-            buffer[6] &= 0x4f //buffer[6] & 01001111 will set the 4, 5, and 7 bit to 0 such that bits 4-7 == 0100 = "4".
-            //buffer[8] represents the clock_seq_hi_and_reserved field. We will set the two most significant bits (6 and 7) of the clock_seq_hi_and_reserved to zero and one, respectively.
-            buffer[8] |= 0x80 //buffer[8] | 10000000 will set the 7 bit to 1.
-            buffer[8] &= 0xbf //buffer[8] & 10111111 will set the 6 bit to 0.
-            return (
-                this._decimalToHex(buffer[0]) +
-                this._decimalToHex(buffer[1]) +
-                this._decimalToHex(buffer[2]) +
-                this._decimalToHex(buffer[3]) +
-                "-" +
-                this._decimalToHex(buffer[4]) +
-                this._decimalToHex(buffer[5]) +
-                "-" +
-                this._decimalToHex(buffer[6]) +
-                this._decimalToHex(buffer[7]) +
-                "-" +
-                this._decimalToHex(buffer[8]) +
-                this._decimalToHex(buffer[9]) +
-                "-" +
-                this._decimalToHex(buffer[10]) +
-                this._decimalToHex(buffer[11]) +
-                this._decimalToHex(buffer[12]) +
-                this._decimalToHex(buffer[13]) +
-                this._decimalToHex(buffer[14]) +
-                this._decimalToHex(buffer[15])
-            )
-        } else {
-            var guidHolder = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
-            var hex = "0123456789abcdef"
-            var r = 0
-            var guidResponse = ""
-            for (var i = 0; i < 36; i++) {
-                if (guidHolder[i] !== "-" && guidHolder[i] !== "4") {
-                    // each x and y needs to be random
-                    r = (Math.random() * 16) | 0
-                }
-                if (guidHolder[i] === "x") {
-                    guidResponse += hex[r]
-                } else if (guidHolder[i] === "y") {
-                    // clock-seq-and-reserved first hex is filtered and remaining hex values are random
-                    r &= 0x3 // bit and with 0011 to set pos 2 to zero ?0??
-                    r |= 0x8 // set pos 3 to 1 as 1???
-                    guidResponse += hex[r]
-                } else {
-                    guidResponse += guidHolder[i]
-                }
+        var buffer = new Uint8Array(16)
+        cryptoObj.getRandomValues(buffer)
+        //buffer[6] and buffer[7] represents the time_hi_and_version field. We will set the four most significant bits (4 through 7) of buffer[6] to represent decimal number 4 (UUID version number).
+        buffer[6] |= 0x40 //buffer[6] | 01000000 will set the 6 bit to 1.
+        buffer[6] &= 0x4f //buffer[6] & 01001111 will set the 4, 5, and 7 bit to 0 such that bits 4-7 == 0100 = "4".
+        //buffer[8] represents the clock_seq_hi_and_reserved field. We will set the two most significant bits (6 and 7) of the clock_seq_hi_and_reserved to zero and one, respectively.
+        buffer[8] |= 0x80 //buffer[8] | 10000000 will set the 7 bit to 1.
+        buffer[8] &= 0xbf //buffer[8] & 10111111 will set the 6 bit to 0.
+        buffer = buffer.map((n: any) => {
+            let hex = n.toString(16)
+            while (hex.length < 2) {
+                hex = "0" + hex
             }
-            return guidResponse
-        }
+            return hex
+        })
+        return (
+            buffer[0] +
+            buffer[1] +
+            buffer[2] +
+            buffer[3] +
+            "-" +
+            buffer[4] +
+            buffer[5] +
+            "-" +
+            buffer[6] +
+            buffer[7] +
+            "-" +
+            buffer[8] +
+            buffer[9] +
+            "-" +
+            buffer[10] +
+            buffer[11] +
+            buffer[12] +
+            buffer[13] +
+            buffer[14] +
+            buffer[15]
+        )
     }
 
     /**
